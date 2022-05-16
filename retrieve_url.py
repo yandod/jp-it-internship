72837c19ac8d95806bc7f71ac954836c30818437
Yusuke Ando <yandod@gmail.com>
2022-05-16 17:53:03 +0900

    replace api

--- retrieve_url.py
+++ retrieve_url.py
@@ -2,7 +2,12 @@
 
 from pprint import pprint
 import json
-from googlesearch import search
+from googleapiclient.discovery import build
+import os
+from time import sleep
+
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+CUSTOM_SEARCH_ENGINE_ID = os.environ['GOOGLE_SEARCH_ENGINE']
 
 json_file = './data/output.json'
 
@@ -12,20 +17,29 @@ with open(json_file) as f:
 total = len(companies['data'])
 print('num of companies:' + str(total))
 
-i = 0;
-skip = 475
+i = 0
+skip = 475 #475
+
+google = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
 for company in companies['data']:
-    i += 1
     print("processing " + str(i) + " of " + str(total))
     if i < skip:
+        i += 1
         continue
-    for result in search(company['name']):
-        pprint(result)
-        companies['data'][i]['url'] = result
-        break
-    if i % 25:
+    pprint(company['name'])
+    sleep(1)
+    response = google.cse().list(
+        q=company['name'],
+        cx=CUSTOM_SEARCH_ENGINE_ID,
+        lr='lang:ja',
+        num=3
+    ).execute()
+    url = response['items'][0]['link']
+    companies['data'][i]['url'] = url
+    if i % 10 == 0:
         with open(json_file, 'w') as f:
             json.dump(companies, f, indent=4, ensure_ascii=False)
+    i += 1
 
 with open(json_file, 'w') as f:
     json.dump(companies, f, indent=4, ensure_ascii=False)
